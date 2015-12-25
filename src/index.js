32f8d99bae53776f50f50aaff99e4edb635f7104
Tom Ruttle <tom@tomruttle.com>
2015-12-25 12:25:08 +0000

    neater syntax using object-path

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
 import SpotifyWebApi from 'spotify-web-api-node';
 import _ from 'highland';
+import op from 'object-path';
 
 import credentials from '../secrets/credentials';
 
-// credentials are optional
 const spotifyApi = new SpotifyWebApi(credentials);
 
-// Retrieve an access token.
 const playlist = spotifyApi.clientCredentialsGrant()
   .then((data) => { spotifyApi.setAccessToken(data.body['access_token']); })
   .then(() => spotifyApi.getPlaylist('musicforants', '0B2xvQtyS1v9wTPoPyJaco'));
 
 _(playlist)
-  .pluck('body')
-  .pluck('tracks')
-  .pluck('items')
+  .map((data) => op.get(data, 'body.tracks.items'))
   .flatten()
   .pluck('track')
   .flatMap(({ id, name, album, artists }) => _(spotifyApi.getAlbum(album.id))
     .pluck('body')
     .filter(({ release_date }) => new Date(release_date).getFullYear() === new Date().getFullYear())
-    .map((data) => ({
-      id,
-      name,
-      albumName: data.name,
-      artist: artists[0].name
-    })))
-  .toArray((x) => { console.log(x); })
+    .map((data) => ({ id, name, album: data.name, artist: artists[0].name })))
+  .each((x) => { console.log(x); })
